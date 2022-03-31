00496a78b2b0e71cc51bb406fb4c347caa790e16
kedabi <taingkn@gmail.com>
2022-03-31 13:58:23 +0200

    change some css

--- src/App.js
+++ src/App.js
@@ -1,27 +1,25 @@
 import * as React from "react";
 import { ethers } from "ethers";
-import './App.css';
+import "./App.css";
 
 export default function App() {
+  const wave = () => {};
 
-  const wave = () => {
-    
-  }
-  
   return (
     <div className="mainContainer">
-
       <div className="dataContainer">
-        <div className="header">
-        👋 Hey there!
-        </div>
+        <div className="header">Bonjour !</div>
 
         <div className="bio">
-        I am farza and I worked on self-driving cars so that's pretty cool right? Connect your Ethereum wallet and wave at me!
+          I am Kevin and I want to track how many adresses wave at me. Connect
+          your Ethereum wallet and dont be shy !
         </div>
 
         <button className="waveButton" onClick={wave}>
-          Wave at Me
+          Say hello{" "}
+          <span role="img" aria-label="wave">
+            👋
+          </span>
         </button>
       </div>
     </div>
